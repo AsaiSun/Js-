b8e5f35514252deeb50a277d0d1555a2d5c18f2f
Asai <as09max@gmail.com>
2025-10-18 17:13:26 +0600

    1

---
+++ Задания/2(3).1.js
@@ -0,0 +1,131 @@
+firstname = "tolek"
+lastname = 'tolan'
+country = "Bibizyania"
+city = "Palma"
+age = "11"
+isMarried = "Yes :("
+year = "~-2000"
+
+if ("10" == 10){
+console.log('true')
+};
+
+a=9,8;
+if (a == 10){
+console.log('true')
+}else{
+    a = 10
+    console.log("теперь true", a)
+};
+
+ljasdhlnjk = Boolean(false);
+asddddd = Boolean(false);
+wfdgdhfjhgj = Boolean(true);
+vdbfklmn = Boolean(true);
+htdfgjl = Boolean(true);
+asdfs = Boolean(false);
+
+a = 4>3;
+console.log(a);
+
+a = 4>=3;
+console.log(a);
+
+
+a = 4<3;
+console.log(a);
+
+
+a = 4<=3;
+console.log(a);
+
+
+a = 4==4;
+console.log(a);
+
+
+a = 4===4;
+console.log(a);
+
+
+a = 4!=4;
+console.log(a);
+
+
+a = 4!==4;
+console.log(a);
+
+
+a = 4!="4";
+console.log(a);
+
+
+a = 4=="4";
+console.log(a);
+
+
+a = 4==="4";
+console.log(a);
+
+console.log("");
+a = 4>3 && 10<12;
+console.log(a);
+
+
+a = 4>3 && 10>12;
+console.log(a);
+
+
+a = 4>3 || 10<12;
+console.log(a);
+
+
+a = 4>3 || 10>12;
+console.log(a);
+
+
+a = !(4>3);
+console.log(a);
+
+a = !(4<3);
+console.log(a);
+
+a = !(false);
+console.log(a);
+
+
+a = !(4>3 && 10<12);
+console.log(a);
+
+
+a = !(4>3 && 10>12);
+console.log(a);
+
+
+a = !(4==="4");
+console.log(a);
+
+a = "python"
+a = a.includes('on')
+console.log(a)
+
+console.log("");
+const now = new Date()
+console.log(now.getFullYear())
+console.log(now.getMonth(), now.getDate())
+console.log(now.getDay())
+
+console.log(now.getDay(), now.getDate())
+console.log(now.getHours()) 
+console.log(now.getMinutes())
+const allSeconds = Date.now() 
+console.log(allSeconds) 
+
+
+
+
+
+
+
+
+
