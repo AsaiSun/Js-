e5c5edcde8c2c8b1e13f6e04d1eeb561f0519104
Asai <as09max@gmail.com>
2025-10-18 15:48:08 +0600

    1

---
+++ Задания/1.1.js
@@ -0,0 +1,21 @@
+let a ="30 дней JavaScript";
+console.log(a);
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.substring(3))
+console.log(a.includes('Script'));
+console.log(a.split(" "));
+b = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+console.log(b.split(","))
+console.log(a.replace("JavaScript", "Python"));
+console.log(a.charAt(15));
+console.log(a.charCodeAt("J"));
+console.log(a.indexOf());
+console.log(a.lastIndexOf());
+console.log(a.trim());
+console.log(a.startsWith("30 дней JavaScript"));
+console.log(a.endsWith("30 дней JavaScript"));
+console.log(a.match("a"));
+console.log(a.concat(" 30 дней", "JavaScript"));
+console.log(a.repeat(2));
---
+++ Задания/1.2.js
@@ -0,0 +1,36 @@
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+if ("10" == 10){
+console.log('true')
+};
+
+a=9,8;
+if (a == 10){
+console.log('true')
+}else{
+    a = 10
+    console.log("теперь true", a)
+};
+a = "python"
+console.log(a.includes('on'));
+a = "I hope this course is not full of jargon"
+console.log(a.includes('if jargon is'));
+console.log(Math.round(Math.random() * (100 - 0)) );
+console.log(Math.round(Math.random() * (100 - 50)) );
+console.log(Math.round(Math.random() * (255 - 0)) );
+a = "   JavaScript"
+console.log(a.charAt(Math.round(Math.random() * (13 - 0))));
+
+for (let i = 1; i<6; i++) {
+    console.log(i, Math.pow(i, 0), i, Math.pow(i, 2), Math.pow(i, 3))
+}
+
+a = "You cannot end a sentence with because because because is a conjunction "
+console.log(a.replace("because because because", ''))
+
+
+
+
+
+
+
